d18e260567323b3992171c2b29caeccc5beee1bb
Javier <javierrsanchezzsoleraa@gmail.com>
2025-10-03 17:10:06 +0200

    Netflix

--- src/bucle.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$variable1=76;
-
-for ($i=0; $i<$variable1; $i++) {
-    echo $i;
-}
---
+++ src/index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Netflix</title>
+    <link rel="stylesheet" href="views/css/cuenta.css" /><!-- Aplico css externo -->
+    <link rel="shortcut icon" type="image/jpg" href="views/img/netflix-flavicon.png" />
+    <meta name="keywords" content="html,css,clon netflix" />
+    <meta name="description"
+          content="Clon del login de usuario de Netflix España hecho con HTML y CSS para la asignatura de Lenguajes de Marcas del IES Fernando Wirtz" />
+    <meta name="author" content="Cristina Correa" />
+    <meta name="generator" content="VSCode" />
+    <meta name="theme-color" content="#000" />
+
+    <meta name="google" content="nositelinkssearchbox" />
+    <meta name="google" content="notranslate" />
+    <meta name="robots" content="index, nofollow" />
+
+
+    <meta property="og:title" content="Clon de Netflix España con HTML y CSS">
+    <meta property="og:description"
+          content="Clon del login de usuario de Netflix España hecho con HTML y CSS para la asignatura de Lenguajes de Marcas del IES Fernando Wirtz">
+</head>
+<body>
+<div class="contenido">
+    <nav>
+        <img class="logo" src="views/img/logo.png" alt="netflixLogo" />
+    </nav>
+    <div class="caja">
+        <h2>Iniciar sesión</h2>
+        <div class="form">
+            <input
+                    type="text"
+                    placeholder="Correo electrónico o número de teléfono"
+                    required
+            />
+            <input type="password" placeholder="Contraseña" required />
+        </div>
+        <a href="./views/welcome.php"><button>Iniciar sesión</button></a>
+        <div class="checkbox">
+            <div class="recordar">
+                <input type="checkbox" id="checkbox1"/>
+                <label for="remember">Recuérdame</label>
+            </div>
+            <div>
+                <p>¿Necesitas ayuda?</p>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+<footer>
+    <p>¿Preguntas? Llama al 900-759-106</p>
+    <div class="links">
+        <ul>
+            <li><a href="#">Preguntas frecuentes</a></li>
+            <li><a href="#">Centro de ayuda</a></li>
+            <li><a href="/terminos-de-uso.html">Términos de uso</a></li>
+            <li><a href="/privacidad.html">Privacidad</a></li>
+            <li><a href="#">Preferencias de cookies</a></li>
+            <li><a href="/info-corporativa.html">Información corporativa</a></li>
+        </ul>
+    </div>
+</footer>
+</html>
